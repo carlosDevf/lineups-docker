e1a751516d3a4e6b7337304823612f1387445b17
cchavezmx99 <carlos.chavez@99minutos.com>
2025-07-05 01:58:14 -0600

    feat: se añade al repo primer init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Dockerfile
+FROM node:latest
+
+# Crear un directorio de trabajo
+WORKDIR /usr/src/app
+
+# Copiar package.json y package-lock.json
+COPY package*.json ./
+
+# Instalar dependencias
+RUN npm install
+
+# Copiar el resto de la aplicación
+COPY . .
+
+# Comando por defecto
+CMD ["npm", "run", "start"]
+
---
+++ main.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import "dotenv/config";
+
+const api_url = process.env.API_URL;
+
+const getDevfToken = async () => {
+  const url = api_url + "api/v1/login";
+  const payload = {
+    username: process.env.DEVF_USERNAME,
+    password: process.env.DEVF_PASSWORD,
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    Origin: "https://thebrain.devf.la",
+  };
+
+  return axios.post(url, payload, { headers });
+};
+
+const lineupCron = async (token) => {
+  let config = {
+    method: "post",
+    url: api_url + "api/v1/lineup/sensei",
+    headers: {
+      Origin: "https://thebrain.devf.la",
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+async function main() {
+  const token = await getDevfToken();
+  if (token.data.token) {
+    await lineupCron(token.data.token);
+  }
+}
+
+main();
